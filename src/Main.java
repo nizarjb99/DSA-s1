87b418e1f0a0810bbf3868c85147aaca69a78a01
RogerMacia <roger.macia@estudiantat.upc.edu>
2025-09-15 16:13:01 +0200

    function sort

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,78 @@
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    static double sum() {
+    public static abstract class Shape implements Comparable<Shape> {
+        public abstract double area();
+
+        public int compareTo(Shape s) {
+            return Double.compare(this.area(), s.area());
+        }
+    }
+    public static class Rectangle extends Shape {
+        private double l1;
+        private double l2;
+
+        public Rectangle(double l1, double l2) {
+            this.l1 = l1;
+            this.l2 = l2;
+        }
+
+        public double area() {
+            return l1 * l2;
+        }
+    }
+
+    public static class Circle extends Shape {
+        private double r;
+
+        public Circle(double r) {
+            this.r = r;
+        }
+
+        public double area() {
+            return Math.PI * r * r;
+        }
+    }
+
+    public static class Triangle extends Shape {
+        private double b;
+        private double h;
+
+        public Triangle(double b, double h) {
+            this.b = b;
+            this.h = h;
+        }
+
+        public double area() {
+            return b * h / 2;
+        }
+    }
+
+    static double sum(Shape[] s) {
 
     }
 
+    static void sort(Shape[] v) {
+        int i = 0;
+        for(Shape s : v) {
+            boolean found = false;
+            int j = 0;
 
+            while (!found || j >= i) {
+                if (s.compareTo(v[j]) < 0) {
+                    found = true;
+
+                    v[i] = v[j];
+                    v[j] = s;
+                }
+                j++;
+            }
+            i++;
+        }
+    }
 
     public static void main(String[] args) {
         Shape[] v = new Shape[4];
